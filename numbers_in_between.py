333936c6c684a772ed2d2ecef8e785e269a9df94
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-08 23:09:11 +0800

    fix: add int function back

--- numbers_in_between.py
+++ numbers_in_between.py
@@ -1,6 +1,6 @@
 # Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
 
-lower_boundary = (input("Enter lower boundary of range: "))
+lower_boundary = int(input("Enter lower boundary of range: "))
 upper_boundary = int(input("Enter upper boundary of range: "))
 
 for num in range(lower_boundary + 1, upper_boundary):
