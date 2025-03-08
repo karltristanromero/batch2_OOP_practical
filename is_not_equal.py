5140b000d87e887a8f5e1eae74fc34a6176ea0e2
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-08 22:43:59 +0800

    style: change print output

--- is_not_equal.py
+++ is_not_equal.py
@@ -4,4 +4,4 @@ first_number = float(input("Enter the first number: "))
 second_number = float(input("Enter the second number: "))
 
 if first_number != second_number:
-    print("Not Equal.")
+    print(f"Numbers {first_number} and {second_number} are not equal.")
