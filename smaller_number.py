6b58a2454ccba11d17957a8a959dd449379d5b8e
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-08 22:35:06 +0800

    style: change variable name for better description

--- smaller_number.py
+++ smaller_number.py
@@ -1,6 +1,6 @@
 # Prog01: Create a program that ask user to input 2 numbers. Print the smaller number.
 
-x = float(input("Enter first number: "))
-y = float(input("Enter second number: "))
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
 
-print(f"{x if x < y else y} is smaller.")
+print(f"{first_number if first_number < second_number else second_number} is smaller.")
