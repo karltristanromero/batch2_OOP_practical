3271b1c892714cac54a5873e1c29634984f805b2
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-08 22:45:13 +0800

    style: change variable names of inputs for better description

--- subtract_numbers.py
+++ subtract_numbers.py
@@ -1,6 +1,6 @@
  # Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
 
-x = float(input("Enter first number: "))
-y = float(input("Enter second number: "))
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
 
-print(f"The difference of the numbers is {x - y}.")
+print(f"The difference of the numbers is {first_number - second_number}.")
