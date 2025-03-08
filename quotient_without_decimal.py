c003df16db0418d71291a7752f4a9b026f6d1109
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-08 22:48:12 +0800

    style: change variable name of input for code readability

--- quotient_without_decimal.py
+++ quotient_without_decimal.py
@@ -1,10 +1,10 @@
 # Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
 
-x = float(input("Enter dividend: "))
-y = float(input("Enter divisor: "))
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
 
 try:
-    quotient = x / y
+    quotient = first_number / second_number
     print(f"The quotient of the numbers is {int(quotient)}.")
 except ZeroDivisionError:
     print("You cannot divide by zero!")
