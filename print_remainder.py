c0f410509e500e121f96822f25609637acdf7204
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-08 22:49:52 +0800

    style: change variable name of result for code readability

--- print_remainder.py
+++ print_remainder.py
@@ -1,10 +1,10 @@
 # Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
  
-x = float(input("Enter first number: "))
-y = float(input("Enter second number: "))
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
 
 try:
-    remainder = x % y
+    remainder = first_number % second_number
     print(f"The remainder of the quotient is {remainder}.")
 except ZeroDivisionError:
     print("You cannot divide by zero.")
