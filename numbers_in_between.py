4b3045cc536b5a5060477683b38163fcafdef85a
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-08 23:08:14 +0800

    style: change variable names of inputs and index for better code readability

--- numbers_in_between.py
+++ numbers_in_between.py
@@ -1,7 +1,7 @@
 # Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
 
-lb = int(input("Enter lower boundary of range: "))
-ub = int(input("Enter upper boundary of range: "))
+lower_boundary = (input("Enter lower boundary of range: "))
+upper_boundary = int(input("Enter upper boundary of range: "))
 
-for i in range(lb + 1, ub):
-    print(i, end= " ")
+for num in range(lower_boundary + 1, upper_boundary):
+    print(num, end= " ")
